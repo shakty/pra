10ccda4363ef6cc9eebe01820ae2e2defa02bbc8
Stefano Balietti <futur.dorko@gmail.com>
2013-02-16 18:11:58 +0100

    mods

--- R/PR.ggplot2.R
+++ R/PR.ggplot2.R
@@ -1,10 +1,99 @@
 #source('PR_init.R')
 
 library(ggplot2)
+library(reshape)
 
 setwd("/var/www/pra/data/ALL/")
 
 pr <- read.table(file="./all.csv", head=TRUE, sep=",")
+
+pr.clean <- na.omit(pr)
+
+all.rounds.d.pub.previous <- tapply(pr.clean$d.pub.previous, pr.clean$round, mean)
+plot.ts(seq(2:30), all.rounds.d.pub.previous)
+
+all.rounds.d.sub.current <- tapply(pr.clean$d.sub.current, pr.clean$round, mean)
+plot.ts(seq(2:30), all.rounds.d.sub.current)
+
+all.rounds.d.self.previous <- tapply(pr.clean$d.self.previous, pr.clean$round, mean)
+plot.ts(seq(2:30), all.rounds.d.self.previous)
+
+all.rounds.d.pub.cumulative <- tapply(pr.clean$d.pub.cumulative, pr.clean$round, mean)
+plot.ts(seq(2:30), all.rounds.d.pub.cumulative)
+
+pr.com.clean <- na.omit(pr[pr$com == 1, ])
+
+com.rounds.d.pub.previous <- tapply(pr.com.clean$d.pub.previous, pr.com.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.previous)
+
+com.rounds.d.sub.current <- tapply(pr.com.clean$d.sub.current, pr.com.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.sub.current)
+
+com.rounds.d.self.previous <- tapply(pr.com.clean$d.self.previous, pr.com.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.self.previous)
+
+com.rounds.d.pub.cumulative <- tapply(pr.com.clean$d.pub.cumulative, pr.com.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.cumulative)
+
+
+pr.coo.clean <- na.omit(pr[pr$coo == 1, ])
+
+com.rounds.d.pub.previous <- tapply(pr.coo.clean$d.pub.previous, pr.coo.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.previous)
+
+com.rounds.d.sub.current <- tapply(pr.coo.clean$d.sub.current, pr.coo.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.sub.current)
+
+com.rounds.d.self.previous <- tapply(pr.coo.clean$d.self.previous, pr.coo.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.self.previous)
+
+com.rounds.d.pub.cumulative <- tapply(pr.coo.clean$d.pub.cumulative, pr.coo.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.cumulative)
+
+
+pr.choice.clean <- na.omit(pr[pr$choice == 1, ])
+
+com.rounds.d.pub.previous <- tapply(pr.choice.clean$d.pub.previous, pr.choice.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.previous)
+
+com.rounds.d.sub.current <- tapply(pr.choice.clean$d.sub.current, pr.choice.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.sub.current)
+
+com.rounds.d.self.previous <- tapply(pr.choice.clean$d.self.previous, pr.choice.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.self.previous)
+
+com.rounds.d.pub.cumulative <- tapply(pr.choice.clean$d.pub.cumulative, pr.choice.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.cumulative)
+
+
+pr.rand.clean <- na.omit(pr[pr$rand == 1, ])
+
+com.rounds.d.pub.previous <- tapply(pr.rand.clean$d.pub.previous, pr.rand.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.previous)
+
+com.rounds.d.sub.current <- tapply(pr.rand.clean$d.sub.current, pr.rand.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.sub.current)
+
+com.rounds.d.self.previous <- tapply(pr.rand.clean$d.self.previous, pr.rand.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.self.previous)
+
+com.rounds.d.pub.cumulative <- tapply(pr.rand.clean$d.pub.cumulative, pr.rand.clean$round, mean)
+plot.ts(seq(2:30), com.rounds.d.pub.cumulative)
+
+
+names.pr <- names(pr)
+
+pr.melted <- melt(pr, id=names.pr[c(-64, -70, -75)])
+pr.melted.clean <- na.omit(pr.melted)
+
+#com.rounds.d.pub.cumulative <- tapply(pr.melted.clean$value, pr.rand.clean$, mean)
+#plot.ts(seq(2:30), com.rounds.d.pub.cumulative)
+
+pr.factor <- pr
+pr.factor$e1.same.ex <- as.factor(pr.factor$e1.same.ex)
+
+aggdata <- aggregate(pr.factor, by=list(pr.factor$e1,pr.factor$e1.same.ex), FUN=mean, na.rm=TRUE)
+
 
 #summary(pr)
 #names(pr)
@@ -24,6 +113,11 @@ filterVars <- function(pattern, varlist) {
   return(selected)
 }
 
+require(plyr)
+
+ddply(pr, "round", transform, grp.mean.values = mean(d.sub.current))
+
+grp.mean.values <- ave(pr$d.sub.current, pr$round)
 
 #meanRounds <- column
 
@@ -57,29 +151,85 @@ srt=90, adj=c(1,0.5), xpd=NA, ... )
 }
 
 
+mysession <- pr[pr$session == 3, ]
 
-face <- pr[,13:25]
-face.norm <- pr[,26:38]
+#face <- face[face$round == 4, 13:25]
+#face.norm <- face[face$round == 4, 26:38]
 
-face <- pr[pr$round == 4, 13:25]
-face.norm <- pr[pr$round == 4, 26:38]
+players.labels=seq(2:10)+1
+sessions.ids <- seq(1,8)
 
-face.matrix <- as.matrix(face)
-face.norm.matrix <- as.matrix(face.norm)
+sessions <- unique(pr$session)
+#id=seq(1,30*length(sessions))
 
-face.dist <- dist(face)
-face.norm.dist <- dist(face.norm)
+cutoff = 30
 
-face.hclust <- hclust(face.dist, method="average")
-face.norm.hclust <- hclust(face.norm.dist,labels=seq(2:10)+1 )
+groups.matrix <- vector()
+groups.max <- vector()
+groups.metadata <- vector()
+for (session in sessions) {
+  mysession <- pr[pr$session == session, ]
+  mysession.groups <- vector()
+  mysession.max <- vector()
+  mysession.metadata <- data.frame()
+  for (round in seq(1:30)) {
+    # perform Hierchical clustering and cut the tree at "cutoff"
+    round.groups <- countGroups(mysession, round, cutoff)
+    round.ngroups <- max(round.groups)   
+    #round.fulline <- c(mysession.metadata, round.groups, round.ngroups)
+    mysession.groups <- rbind(mysession.groups, round.groups)
+    mysession.max <- rbind(mysession.max, round.ngroups)
+    mysession.metadata <- rbind(mysession.metadata, mysession[1,sessions.ids])
+  }
+  groups.matrix <- rbind(groups.matrix, mysession.groups)
+  groups.max <- rbind(groups.max, mysession.max)
+  groups.metadata <- rbind(groups.metadata, mysession.metadata)
+  #ngroups <- apply(round.groups,1,max)
+  #mygroups.frame <- rbind(mygroups)
+}
 
-plot(face.hclust,labels=seq(2:10)+1)
+groups.metadata$id <- seq(1:nrow(groups.metadata))
 
-plot(face.norm.hclust,labels=seq(2:10)+1)
+groups.frame <- as.data.frame(groups.matrix)
+groups.frame$id <- seq(1:nrow(groups.frame))
 
-heatmap(face.matrix)
+groups.frame <- merge(groups.metadata, groups.frame, by = "id", all = TRUE)
+names(groups.frame)[c(10:18)] <- paste0("p", seq(2:10)+1)
 
-face.groups<-cutree(face.hclust, h=30)
+groups.frame$max <- groups.max
+
+
+for (session in sessions) {
+   mysession <- groups.frame[groups.frame$session == session, ]
+   plot.ts(mysession$max)
+   par(ask=TRUE) 
+}
+
+countGroups <- function(session, round, cutoff) {
+  faces <- session[mysession$round == round,13:25]
+  faces.norm <- session[mysession$round == round, 26:38]
+
+  faces.matrix <- as.matrix(faces)
+  faces.norm.matrix <- as.matrix(faces.norm)
+
+  faces.dist <- dist(faces)
+  faces.norm.dist <- dist(faces.norm)
+
+  faces.hclust <- hclust(faces.dist)
+  faces.norm.hclust <- hclust(faces.norm.dist)
+
+  #plot(faces.hclust,labels=seq(2:10)+1)
+  #plot(faces.norm.hclust,labels=seq(2:10)+1)
+
+  #heatmap(faces.matrix)
+
+  faces.groups<-cutree(faces.hclust, h=cutoff)
+
+  return(faces.groups)
+}
+
+#ngroups <- max(faces.groups)
+  
 
 
 # EXAMPLE
--- R/test.R
+++ R/test.R
@@ -33,3 +33,80 @@ par(mfrow=c(1,3))
 image(t(dataMatrixOrdered)[,nrow(dataMatrixOrdered):1])
 plot(rowMeans(dataMatrixOrdered),40:1,,xlab="Row",ylab="Row Mean",pch=19)
 plot(colMeans(dataMatrixOrdered),xlab="Column",ylab="Column Mean",pch=19)
+
+
+
+library(UsingR); data(galton);
+plot(galton$parent,galton$child,pch=19,col="blue")
+lm1 <- lm(galton$child ~ galton$parent)
+lines(galton$parent,lm1$fitted,col="red",lwd=3)
+
+
+newGalton <- data.frame(parent=rep(NA,1e6),child=rep(NA,1e6))
+newGalton$parent <- rnorm(1e6,mean=mean(galton$parent),sd=sd(galton$parent))
+newGalton$child <- lm1$coeff[1] + lm1$coeff[2]*newGalton$parent + rnorm(1e6,sd=sd(lm1$residuals))
+smoothScatter(newGalton$parent,newGalton$child)
+abline(lm1,col="red",lwd=3)
+
+set.seed(134325); sampleGalton1 <- newGalton[sample(1:1e6,size=50,replace=F),]
+sampleLm1 <- lm(sampleGalton1$child ~ sampleGalton1$parent)
+plot(sampleGalton1$parent,sampleGalton1$child,pch=19,col="blue")
+lines(sampleGalton1$parent,sampleLm1$fitted,lwd=3,lty=2)
+abline(lm1,col="red",lwd=3)
+
+sampleLm <- vector(100,mode="list")
+for(i in 1:100){
+sampleGalton <- newGalton[sample(1:1e6,size=50,replace=F),]
+sampleLm[[i]] <- lm(sampleGalton$child ~ sampleGalton$parent)
+}
+
+
+par(mar=c(4,4,0,2));plot(1:10,type="n",xlim=c(0,1.5),ylim=c(0,100),
+xlab="Coefficient Values",ylab="Replication")
+for(i in 1:100){
+ci <- confint(sampleLm[[i]]); color="red";
+if((ci[2,1] < lm1$coeff[2]) & (lm1$coeff[2] < ci[2,2])){color = "grey"}
+segments(ci[2,1],i,ci[2,2],i,col=color,lwd=3)
+}
+lines(rep(lm1$coeff[2],100),seq(0,100,length=100),lwd=3)
+
+x <- seq(-20,20,length=100)
+plot(x,dt(x,df=(928-2)),col="blue",lwd=3,type="l")
+arrows(summary(lm1)$coeff[2,3],0.25,summary(lm1)$coeff[2,3],0,col="red",lwd=4)
+
+
+## Regressions with factors
+
+download.file("http://www.rossmanchance.com/iscam2/data/movies03RT.txt",destfile="./data/movies.txt"
+movies <- read.table("./data/movies.txt",sep="\t",header=T,quote="")
+head(movies)
+
+
+plot(movies$score ~ jitter(as.numeric(movies$rating)),col="blue",xaxt="n",pch=19)
+
+              
+axis(side=1,at=unique(as.numeric(movies$rating)),labels=unique(movies$rating))
+
+
+lm1 <- lm(movies$score ~ as.factor(movies$rating))
+summary(lm1)
+
+
+lm2 <- lm(movies$score ~ relevel(as.factor(movies$rating),ref="R"))
+summary(lm2)
+
+anova(lm1)
+
+lm1 <- aov(movies$score ~ as.factor(movies$rating))
+TukeyHSD(lm1)
+
+              
+
+# Multiple regression
+
+download.file("http://apps.who.int/gho/athena/data/GHO/WHOSIS_000008.csv?profile=text&filter=COUNTRY:
+hunger <- read.csv("./data/hunger.csv")
+hunger <- hunger[hunger$Sex!="Both sexes",]
+head(hunger)
+
+              
