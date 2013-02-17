81da4e6e8daaf135098120a4a86932566c0b513e
Stefano Balietti <futur.dorko@gmail.com>
2013-02-17 23:17:16 +0100

    mods

--- R/PR.ggplot2.R
+++ R/PR.ggplot2.R
@@ -2,6 +2,7 @@
 
 library(ggplot2)
 library(reshape)
+library(MASS)
 
 setwd("/var/www/pra/data/ALL/")
 
@@ -83,8 +84,70 @@ plot.ts(seq(2:30), com.rounds.d.pub.cumulative)
 
 names.pr <- names(pr)
 
-pr.melted <- melt(pr, id=names.pr[c(-64, -70, -75)])
+pr.melted <- melt(pr, id=names.pr[c(-64, -70, -76)])
+
 pr.melted.clean <- na.omit(pr.melted)
+
+vars.keep <- c("coo","com","rand","choice","e1.same.ex", "e1.same.color", "e2.same.ex", "e2.same.color", "e3.same.ex", "e3.same.color")
+
+pr.melted <- melt(pr, measure.vars=c("e1","e2","e3", "e1.same.ex", "e1.same.color", "e2.same.ex", "e2.same.color", "e3.same.ex", "e3.same.color"), id.vars=vars.keep)
+head(pr.melted)
+
+#evas <- data.frame(row.names = c("session", "coo", "com", "rand", "choice", "order", "value", "same.ex", "same.color"))
+evas <- data.frame()
+for (e in c("e1", "e2", "e3")) {
+  mydata <- pr.melted[pr.melted$variable == e, ]
+  sameex <- paste0(e, ".same.ex")
+  samecol <- paste0(e, ".same.color")
+  sameex.column <- with(mydata, get(sameex))
+  samecol.column <- with(mydata, get(samecol))
+  metadata <- mydata[,sessions.ids]
+  myeva <- data.frame(metadata,
+                      round=mydata$round,
+                      order=mydata$variable, value=mydata$value,
+                      same.ex=sameex.column, same.color=samecol.column)  
+  evas <- rbind(evas, myeva) 
+}
+
+p.evas <- ggplot(evas, aes(x=value, group=com, colour=com))
+
+#p.line.com <- p + aes(colour=com) + geom_line(); p.line.com
+#p.line.rand <- p + aes(colour=rand) + geom_line(); p.line.rand
+
+p.evas.density <- p.evas + geom_density()
+p.evas.density
+
+p.evas.hist <- p.evas + geom_histogram(aes(y=..density..),      # Histogram with density instead of count on y-axis
+                   binwidth=.5)
+
+
+p.evas.boxplot <- p.evas + geom_boxplot(aes(x=com,y=value))
+
+#+ geom_density(alpha=.2, fill="#FF6666")  # Overlay with transparent density plot
+
+
+p.evas.facets <- p.evas + aes(colour=com) + geom_boxplot()
+
+summary(evas[evas$com==1,])
+summary(evas[evas$com==0,])
+
+
+evas.t.com <- t.test(value ~ com, data=evas)
+
++ facet_grid(rand ~ com, margins = T);
+
+p.evas.facets
+
+p.facets <- p.facets + opts(title="Number of clusters per round per treatment condition")
+#p.facets  + opts(strip.text.x = theme_text(size = 8, colour = "red", angle = 90))
+
+boxplot(evas$value ~ evas$same.ex)
+
+
+
+
+#cast(pr.melted2, value ~ e1.same.ex)
+
 
 #com.rounds.d.pub.cumulative <- tapply(pr.melted.clean$value, pr.rand.clean$, mean)
 #plot.ts(seq(2:30), com.rounds.d.pub.cumulative)
@@ -151,72 +214,39 @@ srt=90, adj=c(1,0.5), xpd=NA, ... )
 }
 
 
-mysession <- pr[pr$session == 3, ]
-
-#face <- face[face$round == 4, 13:25]
-#face.norm <- face[face$round == 4, 26:38]
-
-players.labels=seq(2:10)+1
-sessions.ids <- seq(1,8)
-
-sessions <- unique(pr$session)
-#id=seq(1,30*length(sessions))
-
-cutoff = 30
-
-groups.matrix <- vector()
-groups.max <- vector()
-groups.metadata <- vector()
-for (session in sessions) {
-  mysession <- pr[pr$session == session, ]
-  mysession.groups <- vector()
-  mysession.max <- vector()
-  mysession.metadata <- data.frame()
+countGroupsInSession <- function(session, cutoff) {
+  
+  session.groups <- matrix(nrow=30, ncol=9, dimnames=list(rnames, pnames))
+  session.max <- matrix(nrow=30, ncol=1, dimnames=list(rnames, "max"))
+  session.metadata <- data.frame()
   for (round in seq(1:30)) {
     # perform Hierchical clustering and cut the tree at "cutoff"
-    round.groups <- countGroups(mysession, round, cutoff)
+    round.groups <- countGroups(session, round, cutoff)
     round.ngroups <- max(round.groups)   
-    #round.fulline <- c(mysession.metadata, round.groups, round.ngroups)
-    mysession.groups <- rbind(mysession.groups, round.groups)
-    mysession.max <- rbind(mysession.max, round.ngroups)
-    mysession.metadata <- rbind(mysession.metadata, mysession[1,sessions.ids])
+    session.groups[round,] <- round.groups
+    session.max[round,] <- round.ngroups
+    session.metadata <- rbind(session.metadata, session[1,sessions.ids])
   }
-  groups.matrix <- rbind(groups.matrix, mysession.groups)
-  groups.max <- rbind(groups.max, mysession.max)
-  groups.metadata <- rbind(groups.metadata, mysession.metadata)
-  #ngroups <- apply(round.groups,1,max)
-  #mygroups.frame <- rbind(mygroups)
-}
-
-groups.metadata$id <- seq(1:nrow(groups.metadata))
-
-groups.frame <- as.data.frame(groups.matrix)
-groups.frame$id <- seq(1:nrow(groups.frame))
-
-groups.frame <- merge(groups.metadata, groups.frame, by = "id", all = TRUE)
-names(groups.frame)[c(10:18)] <- paste0("p", seq(2:10)+1)
-
-groups.frame$max <- groups.max
-
-
-for (session in sessions) {
-   mysession <- groups.frame[groups.frame$session == session, ]
-   plot.ts(mysession$max)
-   par(ask=TRUE) 
+  session.frame <- data.frame(session.metadata, round=c(1:30), session.groups, session.max)
+  return(session.frame)
 }
 
 countGroups <- function(session, round, cutoff) {
-  faces <- session[mysession$round == round,13:25]
-  faces.norm <- session[mysession$round == round, 26:38]
-
+  faces <- session[session$round == round,13:25]
+  
+  if (any(is.na(faces))) {
+    return(rep(NA,9))
+  }
+  #faces.norm <- session[session$round == round, 26:38]
+  
   faces.matrix <- as.matrix(faces)
-  faces.norm.matrix <- as.matrix(faces.norm)
+  #faces.norm.matrix <- as.matrix(faces.norm)
 
   faces.dist <- dist(faces)
-  faces.norm.dist <- dist(faces.norm)
+  #faces.norm.dist <- dist(faces.norm)
 
   faces.hclust <- hclust(faces.dist)
-  faces.norm.hclust <- hclust(faces.norm.dist)
+  #faces.norm.hclust <- hclust(faces.norm.dist)
 
   #plot(faces.hclust,labels=seq(2:10)+1)
   #plot(faces.norm.hclust,labels=seq(2:10)+1)
@@ -228,16 +258,88 @@ countGroups <- function(session, round, cutoff) {
   return(faces.groups)
 }
 
-#ngroups <- max(faces.groups)
-  
 
 
-# EXAMPLE
-USArrests.dist<-dist(USArrests)
-hc <- hclust(USArrests.dist, "average")
-plot(hc, hang = -1)
-abline(h=50, col="red", lty="dashed")
-groups<-cutree(hc, h=50)
-USArrests2<-cbind(USArrests, Group=groups)
-head(USArrests2)
-#
+#### START ####
+#mysession <- pr[pr$session == 3, ]
+
+
+pr$com <- as.factor(pr$com)
+pr$coo <- as.factor(pr$coo)
+pr$rand <- as.factor(pr$rand)
+pr$choice <- as.factor(pr$choice)
+pr$session <- as.factor(pr$session)
+pr$date <- as.factor(pr$date)
+pr$morning <- as.factor(pr$morning)
+pr$afternoon <- as.factor(pr$afternoon)
+
+pr$ex <- as.factor(pr$ex)
+pr$published <- as.factor(pr$published)
+pr$copy <- as.factor(pr$copy)
+
+
+players.labels=seq(2:10)+1
+sessions.ids <- seq(1,8)
+
+pnames <- paste0("p", seq(2:10)+1)
+rnames <- paste0("r", seq(1:30))
+
+sessions <- unique(pr$session)
+cutoff = 30
+
+
+
+groups.frame <- data.frame()
+for (s in sessions) {
+  session <- pr[pr$session == s, ]
+  session.frame <- countGroupsInSession(session, cutoff)
+  groups.frame <- rbind(groups.frame, session.frame)
+}
+row.names(groups.frame) <- c(1:nrow(groups.frame))
+head(groups.frame)
+
+p <- ggplot(groups.frame, aes(round, max, group=session))
+
+p.line.com <- p + aes(colour=com) + geom_line(); p.line.com
+
+p.line.rand <- p + aes(colour=rand) + geom_line(); p.line.rand
+
+p <- ggplot(groups.frame, aes(round, max))
+p.facets <- p + aes(colour=com) + geom_smooth() + facet_grid(rand ~ com, margins = T);
+p.facets <- p.facets + opts(title="Number of clusters per round per treatment condition")
+#p.facets  + opts(strip.text.x = theme_text(size = 8, colour = "red", angle = 90))
+
+p.facets
+
+
+### LM
+
+
+aggregate(groups.frame, by=list(iscom = groups.frame$com), mean, na.rm = TRUE)
+
+
+molted <- melt(groups.frame
+
+mysession <- groups.frame[session == 1,]
+fit <- lm(data = mysession, formula = max ~ round)
+summary(fit)
+
+
+plot(fit)
+
+com.rounds.d.pub.cumulative <- tapply(groups.frame$max, groups.frame$com, lm, formula = max ~ round, data = groups.frame)
+
+
+
+
+
+
+
+## OLD STUFF
+
+#for (s in sessions) {
+#   session <- groups.frame[groups.frame$session == s, ]
+#   main <- sprintf("COM: %s - RAND %s", session$com[1], session$rand[1])
+#   plot.ts(session$max, main=main, ylim=c(1,10))
+#   par(ask=TRUE) 
+#}
